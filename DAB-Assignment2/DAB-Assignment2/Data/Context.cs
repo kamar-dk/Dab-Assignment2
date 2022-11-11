fdfc2829af492335c29f36ec777581727e12e16a
KasperMartensen <tigriss.tiger@gmail.com>
2022-11-11 10:49:08 +0100

    .

--- DAB-Assignment2/DAB-Assignment2/Data/Context.cs
+++ DAB-Assignment2/DAB-Assignment2/Data/Context.cs
@@ -21,7 +21,7 @@ namespace DAB_Assignment2.Data
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //optionsBuilder.UseSqlServer("Data Soruce=0.0.0.0, 1433; Database=Assignment2_AU682545; User ID=sa; Password=Kckm31920; TrustServerCertificate=true; ApplicationIntent=ReadWrite;");
-            optionsBuilder.UseSqlServer("Data Source = localhost; Initial Catalog = Assignment2_AU682545; Persist Security Info = True; User ID = sa; Password = Kckm31920");
+            optionsBuilder.UseSqlServer("Data Source = localhost; Initial Catalog = Assignment2_1_AU682545; Persist Security Info = True; User ID = sa; Password = Kckm31920");
             /*var builder = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.js", false);
